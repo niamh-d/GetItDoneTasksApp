bb83567f0baf707d8c644633e90473266121de71
Niamh <85398097+niamh-d@users.noreply.github.com>
2025-07-31 19:33:49 +0300

    floating btn and dialog

--- app/src/main/java/dev/niamhdoyle/getitdone/MainActivity.kt
+++ app/src/main/java/dev/niamhdoyle/getitdone/MainActivity.kt
@@ -1,13 +1,16 @@
 package dev.niamhdoyle.getitdone
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentActivity
 import androidx.viewpager2.adapter.FragmentStateAdapter
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import com.google.android.material.tabs.TabLayout
 import com.google.android.material.tabs.TabLayoutMediator
 import dev.niamhdoyle.getitdone.databinding.ActivityMainBinding
+import dev.niamhdoyle.getitdone.databinding.DialogAddNewTaskBinding
 
 
 class MainActivity : AppCompatActivity() {
@@ -25,6 +28,25 @@ class MainActivity : AppCompatActivity() {
         TabLayoutMediator(vb.tabs, vb.pager) { tab, position ->
             tab.text = "Tasks"
         }.attach()
+
+        vb.fab.setOnClickListener { showAddNewTaskDialog() }
+    }
+
+    private fun showAddNewTaskDialog() {
+        val dialogBinding = DialogAddNewTaskBinding.inflate(layoutInflater)
+
+        MaterialAlertDialogBuilder(this)
+            .setTitle("Add new task")
+            .setView(dialogBinding.root)
+            .setPositiveButton("Save") { _, _ ->
+                Toast.makeText(
+                    this,
+                    "Your task is: ${dialogBinding.editText.text}",
+                    Toast.LENGTH_LONG
+                ).show()
+            }
+            .setNegativeButton("Cancel", null)
+            .show()
     }
 
     inner class PagerAdapter(activity: FragmentActivity) : FragmentStateAdapter(activity) {
