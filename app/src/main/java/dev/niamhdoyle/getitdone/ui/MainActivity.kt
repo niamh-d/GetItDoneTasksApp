1535a0f727f02d3c1da986177b4ae4e4f276bd23
Niamh <85398097+niamh-d@users.noreply.github.com>
2025-08-01 23:18:06 +0300

    new task save btn disabled if new task title is empty

--- app/src/main/java/dev/niamhdoyle/getitdone/ui/MainActivity.kt
+++ app/src/main/java/dev/niamhdoyle/getitdone/ui/MainActivity.kt
@@ -1,6 +1,8 @@
 package dev.niamhdoyle.getitdone.ui
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
@@ -12,6 +14,7 @@ import dev.niamhdoyle.getitdone.data.GetItDoneDb
 import dev.niamhdoyle.getitdone.databinding.ActivityMainBinding
 import dev.niamhdoyle.getitdone.databinding.DialogAddNewTaskBinding
 import androidx.core.view.isVisible
+import androidx.core.widget.addTextChangedListener
 import dev.niamhdoyle.getitdone.data.Task
 import dev.niamhdoyle.getitdone.data.TaskDao
 import dev.niamhdoyle.getitdone.ui.tasks.TasksFragment
@@ -44,6 +47,12 @@ class MainActivity : AppCompatActivity() {
         DialogAddNewTaskBinding.inflate(layoutInflater).apply {
             val dialog = BottomSheetDialog(this@MainActivity)
             dialog.setContentView(root)
+
+            btnSave.isEnabled = false
+
+            editTextTaskTitle.addTextChangedListener { input ->
+                btnSave.isEnabled = !input.isNullOrEmpty()
+            }
 
             btnShowDetails.setOnClickListener {
                 editTextTaskDetails.visibility =
