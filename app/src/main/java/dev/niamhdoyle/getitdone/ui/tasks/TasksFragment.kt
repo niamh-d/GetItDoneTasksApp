5637eb9b3d265b2bd1ce8b4f855548fe5d61f4ed
Niamh <85398097+niamh-d@users.noreply.github.com>
2025-08-01 12:08:34 +0300

    tasks from db showing in ui

--- app/src/main/java/dev/niamhdoyle/getitdone/ui/tasks/TasksFragment.kt
+++ app/src/main/java/dev/niamhdoyle/getitdone/ui/tasks/TasksFragment.kt
@@ -5,33 +5,34 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import dev.niamhdoyle.getitdone.data.Task
+import dev.niamhdoyle.getitdone.data.GetItDoneDb
+import dev.niamhdoyle.getitdone.data.TaskDao
 import dev.niamhdoyle.getitdone.databinding.FragmentTasksBinding
+import kotlin.concurrent.thread
 
 class TasksFragment : Fragment() {
 
     private lateinit var vb: FragmentTasksBinding
+    private val taskDao: TaskDao by lazy {
+        GetItDoneDb.createDb(requireContext()).getTaskDao()
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-
         vb = FragmentTasksBinding.inflate(inflater, container, false)
-
         return vb.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        vb.recyclerView.adapter = TasksAdapter(
-            tasks = listOf(
-                Task(title = "First task", description = "Description 1"),
-                Task(title = "Second task", description = "Description 2"),
-                Task(title = "Third task", description = "Description 3"),
-                Task(title = "Fourth task", description = "Description 4")
-            )
-        )
+        thread {
+            val tasks = taskDao.getAllTasks()
+            requireActivity().runOnUiThread {
+                vb.recyclerView.adapter = TasksAdapter(tasks = tasks)
+            }
+        }
     }
 }
